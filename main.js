20249ff20d7de2ff99a1de6656a728e270b9803c
tahmid2008ahmed <ahmed2008tahmid@gmail.com>
2024-08-31 15:12:07 +0600

    now the clock will run

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-let date = new Date();
-// setting time
-let time = date.toLocaleTimeString();
-// console.log(time);
+function updateClock() {
+  let now = new Date();
 
-let timeDiv = document.getElementById("time");
-timeDiv.textContent = time;
+  // Update time
+  let time = now.toLocaleTimeString();
+  let timeDiv = document.getElementById("time");
+  timeDiv.textContent = time;
 
-// setting Date
-let todayDate = date.toDateString();
-// console.log(todayDate);
+  // Update date
+  let todayDate = now.toDateString();
+  let dateDiv = document.getElementById("date");
+  dateDiv.textContent = todayDate;
 
-let dateDiv = document.getElementById("date");
-dateDiv.textContent = todayDate;
+  setTimeout(updateClock, 1000);
+}
+
+updateClock();
